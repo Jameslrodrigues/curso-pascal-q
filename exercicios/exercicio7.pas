8917205e9a56d1e843f3c47691141a3b7eff6514
jameslrodrigues <jamesleerodrigues@outlook.com>
2025-09-16 00:18:32 -0300

    Grupo1 - exercício7.pas

--- exercicios/exercicio7.pas
+++ exercicios/exercicio7.pas
@@ -5,7 +5,16 @@
   Criado por: @programacaomentoria
 }  
 program exercicio7;
-
-begin:
-
+var
+    area: real;
+    cumprimento : real;
+    altura : real;
+begin
+  writeln ('-- Iremos calcular a área de um retângulo --');
+  writeln ('Por favor me informe o valor de seu cumprimento ');
+  readln (cumprimento);
+  writeln ('Agora me informe o valor de sua altura ');
+  readln (altura);
+  area := cumprimento*altura;
+  writeln('O valor da área do retêngulo é: ', area:0:2);
 end.
