1453741641591b1ee187a27f90e27249400d3422
jameslrodrigues <jamesleerodrigues@outlook.com>
2025-09-16 00:17:54 -0300

    Grupo1 - exercício3.pas

--- exercicios/exercicio3.pas
+++ exercicios/exercicio3.pas
@@ -4,7 +4,21 @@
   Criado por: @programacaomentoria
 }  
 program exercicio3;
-
-begin:
-
+var
+    nome: string;
+    nota1, nota2, nota3, nota4: real;
+    media: real;
+begin
+  writeln ('Qual é o seu nome? ');
+  readln (nome);
+  writeln ('Qual a primeira nota que você tirou na prova? ');
+  readln (nota1);
+  writeln ('Qual a segunda nota que você tirou na prova? ');
+  readln (nota2);
+  writeln ('Qual a terceira nota que você tirou na prova? ');
+  readln (nota3);
+  writeln ('Qual a quarta nota que você tirou na prova? ');
+  readln (nota4);
+  media := (nota1+nota2+nota3+nota4)/4;
+  writeln (nome, ' a sua média é: ', media:0:2);
 end.
