d15eabcdcf752a61e2fd5fe82410936ff2149852
jameslrodrigues <jamesleerodrigues@outlook.com>
2025-09-16 00:18:04 -0300

    Grupo1 - exercício4.pas

--- exercicios/exercicio4.pas
+++ exercicios/exercicio4.pas
@@ -4,7 +4,12 @@
   Criado por: @programacaomentoria
 }  
 program exercicio4;
-
-begin:
-
+var
+    numero: integer;
+    quadrado: real;
+begin
+  writeln ('Por favor me informe um número ');
+  readln (numero);
+  quadrado := (numero * numero);
+  writeln ('O quadrado de ', numero, ' é: ', quadrado:0:2);
 end.
